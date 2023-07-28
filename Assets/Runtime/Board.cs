96477cf20cc61b48fd3fc239c83414d862a3cda3
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2023-07-28 11:56:45 +0200

    feat: build board

---
+++ Assets/Runtime/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runtime
+{
+    public class Board : MonoBehaviour
+    {
+        [SerializeField]
+        private Square squarePrefab;
+        private List<Square> _squares;
+
+        private void Awake()
+        {
+            _squares = new List<Square>();
+            BuildBoard();
+        }
+
+        private void BuildBoard()
+        {
+            int size = 5;
+            for (int i = -size/2; i <= size/2; i++)
+            {
+                for (int j = -size/2; j <= size/2; j++)
+                {
+                    var square = Instantiate(squarePrefab, new Vector3(i + i * 0.1f, j + j * 0.1f, 0), Quaternion.identity);
+                    _squares.Add(square);
+                }
+            }
+        }
+    }
+}
--- Assets/Runtime/Card.cs
+++ Assets/Runtime/Card.cs
@@ -1,44 +1,46 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /*
-  Player, Hand, Deck, Card, Trait, Board, Connection, Square, Happiness, Score, Milestone, character, terrain
+  Player, Hand, Deck, Card, Trait, Board, Connection, Square, Happiness, Milestone, Character, Terrain
   
+  Build Board
+  Build Deck
+  Draw Cards
   Put Card in Square
   Compare Traits between squares with cards
   Calculate happiness in Connection
+  Calculate total happiness
   
-   
  */
-public class Card : MonoBehaviour
-{
-    private bool _grabbed;
-    
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast (ray, out var hit, 100))
-            {
-                _grabbed = true;
-            }
-        }
-        else if(_grabbed && Input.GetMouseButtonUp(0))
-        {
-            _grabbed = false;
-        }
 
-        if (_grabbed)
+namespace Runtime
+{
+    public class Card : MonoBehaviour
+    {
+        private bool _grabbed;
+    
+        private void Update()
         {
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition.z = 10;
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePosition);
-            Debug.Log(Input.mousePosition);
-            Debug.Log(mouseWorldPos);
-            transform.position = mouseWorldPos;
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast (ray, out var hit, 100))
+                {
+                    _grabbed = true;
+                }
+            }
+            else if(_grabbed && Input.GetMouseButtonUp(0))
+            {
+                _grabbed = false;
+            }
+
+            if (_grabbed)
+            {
+                Vector3 mousePosition = Input.mousePosition;
+                mousePosition.z = 5;
+                Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePosition);
+                transform.position = mouseWorldPos;
+            }
         }
     }
 }
