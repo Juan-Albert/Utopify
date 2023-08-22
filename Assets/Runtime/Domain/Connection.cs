0ff06382d88114b948a8cc33b0d0893c5b936610
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2023-08-22 14:35:44 +0200

    test: inverted connections are equal

--- Assets/Runtime/Domain/Connection.cs
+++ Assets/Runtime/Domain/Connection.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace Runtime.Domain
 {
-    public class Connection
+    public class Connection : IEquatable<Connection>
     {
         public Square FromSquare { get; }
         public Square ToSquare { get; }
@@ -19,6 +21,26 @@ namespace Runtime.Domain
         {
             return (FromSquare.Coordinate.Equals(from)&& ToSquare.Coordinate.Equals(to))
                 || (FromSquare.Coordinate.Equals(to)&& ToSquare.Coordinate.Equals(from));
+        }
+
+        public bool Equals(Connection other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.FromSquare.Coordinate, other.ToSquare.Coordinate);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Connection)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_happiness, FromSquare, ToSquare);
         }
     }
 }
---
+++ Assets/Tests/EditMode/ConnectionTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Runtime.Domain;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Tests.EditMode
+{
+    public class ConnectionTests
+    {
+        [Test]
+        public void InvertedConnections_AreEqual()
+        {
+            var fromSquare = new Square(new Coordinate(0, 0));
+            var toSquare = new Square(new Coordinate(1, 0));
+            var sut1 = new Connection(fromSquare, toSquare);
+            var sut2 = new Connection(toSquare, fromSquare);
+            
+            Assert.AreEqual(sut1, sut2);
+        }
+    }
+}
