dbb8fa86600aa074daa28c0f7cdd009a59e75141
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2023-08-23 13:38:01 +0200

    feat: play cards from the player

---
+++ Assets/Runtime/Domain/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace Runtime.Domain
+{
+    public class Player
+    {
+        private int _happiness;
+        private readonly Hand _hand;
+        private readonly Board _board;
+
+        public Player(Hand hand, Board board)
+        {
+            _hand = hand;
+            _board = board;
+        }
+
+        public void PlayCard(Card card, Coordinate coordinate)
+        {
+            _board.PlayCard(card, coordinate);
+            _hand.PlayCard(card);
+            _happiness = _board.GetBoardHappiness();
+        }
+    }
+}
