e4d69cf9eb7486e6eadb96451f4248563640e849
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2023-08-23 13:17:22 +0200

    feat: play and draw cards in player hand

---
+++ Assets/Runtime/Domain/Hand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace Runtime.Domain
+{
+    public class Hand
+    {
+        private readonly int _handSize;
+        private readonly List<Card> _cards;
+        private readonly Deck _deck;
+
+        public Hand(int handSize, List<Card> cards, Deck deck)
+        {
+            _handSize = handSize;
+            _cards = cards;
+            _deck = deck;
+        }
+
+        public void PlayCard(Card card)
+        {
+            Assert.IsTrue(_cards.Contains(card));
+            _cards.Remove(card);
+
+            if (_cards.Count == 0)
+            {
+                for (int i = 0; i < Math.Min(_handSize, _deck.Cards.Count); i++)
+                {
+                    _cards.Add(DrawCard());
+                }
+            }
+        }
+
+        private Card DrawCard()
+        {
+            return _deck.DrawCard();
+        }
+    }
+}
