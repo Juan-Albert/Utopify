ada3d23058c84e3e0088fc3e2e9a030491d68fc1
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2023-08-23 12:35:18 +0200

    feat: group cards in a deck

---
+++ Assets/Runtime/Domain/Deck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Assertions;
+
+namespace Runtime.Domain
+{
+    public class Deck
+    {
+        private Stack<Card> _cards;
+
+        public Deck(IEnumerable<Card> cards)
+        {
+            _cards = cards as Stack<Card>;
+            Shuffle();
+        }
+
+        public Card DrawCard()
+        {
+            Assert.IsTrue(_cards.Count > 0);
+            return _cards.Pop();
+        }
+
+        public void AddCard(Card card)
+        {
+            Assert.IsNotNull(card);
+            _cards.Push(card);
+            Shuffle();
+        }
+
+        public void ShuffleDeck(IEnumerable<Card> otherDeck)
+        {
+            foreach (var card in otherDeck)
+            {
+                AddCard(card);
+            }
+        }
+        
+        private void Shuffle() 
+        {
+            var random = new Random((int)DateTime.Now.Ticks);
+            var shuffledDeck = _cards.Select(x => new { Number = random.Next(), Card = x })
+                .OrderBy(x => x.Number)
+                .Select(x => x.Card);
+            _cards = shuffledDeck as Stack<Card>;
+        }
+        
+    }
+}
