6762811b8b7839b87e91cde5b888192d33493407
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2023-08-24 14:29:33 +0200

    feat: capture player mouse input

---
+++ Assets/Runtime/View/PlayerInput.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace Runtime.View
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        private bool _listening;
+        private bool _clickDown;
+        
+        public delegate void EventHandler(Vector3 position);
+
+        public event EventHandler OnClickDown;
+        public event EventHandler OnDrag;
+        public event EventHandler OnClickUp;
+
+        private void Update()
+        {
+            if (!_listening)
+                return;
+
+            ClickDown();
+            Drag();
+            ClickUp();
+        }
+
+        public void Setup()
+        {
+            EnableInput(true);
+        }
+
+        private void EnableInput(bool value)
+        {
+            _listening = value;
+        }
+
+        private void ClickDown()
+        {
+            if (!_clickDown)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    _clickDown = true;
+                    OnClickDown?.Invoke(Input.mousePosition);
+                }
+            }
+        }
+
+        private void Drag()
+        {
+            if (_clickDown)
+            {
+                OnDrag?.Invoke(Input.mousePosition);
+            }
+        }
+
+        private void ClickUp()
+        {
+            if (_clickDown)
+            {
+                if (Input.GetMouseButtonUp(0))
+                { 
+                    _clickDown = false;
+                    OnClickUp?.Invoke(Input.mousePosition);
+                }
+            }
+        }
+    }
+}
