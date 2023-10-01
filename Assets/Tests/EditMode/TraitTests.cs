f608c432988d75212c75cac421a1cd9ee5507292
RGV <ricardo.guzman.velasco@outlook.com>
2023-10-01 11:16:05 +0200

    feat: relate friendly traits

---
+++ Assets/Tests/EditMode/TraitTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace Tests.EditMode
+{
+    public class TraitTests
+    {
+        [Test]
+        public void Relationships()
+        {
+            var friend = new Trait();
+            var sut = new Trait(friend);
+            
+            sut.RelationWith(friend).Should().Be(Relationship.Friend);
+        } 
+    }
+
+    public struct Trait
+    {
+        Trait[] friends;
+
+        public Trait(Trait friend)
+        {
+            friends = new[] {friend};
+        }
+
+        public Relationship RelationWith(Trait friend)
+        {
+            return Relationship.Friend;
+        }
+    }
+
+    public enum Relationship { Friend}
+}
