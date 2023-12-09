00545c9f0133ee3d38f40fe762ce8491065e2f7c
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2023-12-09 14:11:56 +0100

    docs: todo next

--- Assets/Tests/EditMode/TraitTests.cs
+++ Assets/Tests/EditMode/TraitTests.cs
@@ -42,8 +42,8 @@ namespace Tests.EditMode
  1. Noción en tablero de qué casillas están disponibles para poner cartas.
     1.1 Siempre las del mapa inicial que no estén ocupadas, DONE
     1.2 más los vecinos de las ocupadas. Sin repetir obviamente dos veces la misma casillas. 
- 2. Noción de si una conexión entre dos casillas es positiva, negativa o neutral. <-
- 3. Vista pa poner cartas. Y que salgan las conexiones.
+ 2. Noción de si una conexión entre dos casillas es positiva, negativa o neutral. 
+ 3. Vista pa poner cartas. Y que salgan las conexiones. <-
  4. Felicidad global.
  5. Mano. Si no hacemos mano al principio, al clicar una casilla se pone una carta cualquiera o lo que sa.
  */
