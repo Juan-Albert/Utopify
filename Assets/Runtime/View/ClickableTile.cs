b9c50e83aa26a89c145e4ed5de005287ec47936e
Iván Moreno <imoreno.owo@gmail.com>
2023-12-09 14:05:01 +0100

    feat: place card by clicking tile

---
+++ Assets/Runtime/View/ClickableTile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Runtime.Domain;
+using UnityEngine;
+
+public class ClickableTile : MonoBehaviour
+{
+    Board board;
+    (int x, int y) whereIs;
+
+    void OnMouseDown()
+    {
+        board.PlaceAt(whereIs, Card.WithTraits(new Trait("random", Array.Empty<string>(), Array.Empty<string>())));
+    }
+
+    public void Configure(Board board, (int x, int y) whereIs)
+    {
+        this.board = board;
+        this.whereIs = whereIs;
+    }
+}
