b8a01db30498875e0ca3d3d8013b0127107a56b1
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2023-12-28 19:03:08 +0100

    feat: add spaces to connections in view

--- Assets/Runtime/View/Connection.cs
+++ Assets/Runtime/View/Connection.cs
@@ -5,8 +5,8 @@ public class Connection : MonoBehaviour
     LineRenderer LineRenderer => GetComponentInChildren<LineRenderer>();
     public void Configure(((int x, int y) from, (int x, int y) to) points)
     {
-        LineRenderer.SetPosition(0, new(points.from.x, points.from.y, 0));
-        LineRenderer.SetPosition(1, new(points.to.x, points.to.y, 0));
+        LineRenderer.SetPosition(0, new(points.from.x + (points.from.x * 0.2f), points.from.y + (points.from.y * 0.2f), 0));
+        LineRenderer.SetPosition(1, new(points.to.x + (points.to.x * 0.2f), points.to.y + (points.to.y * 0.2f), 0));
     }
 
 }
