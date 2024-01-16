a8afc677ca37a6672a91cf728cdca423717d55f4
Iván Moreno <imoreno.owo@gmail.com>
2024-01-16 19:11:18 +0100

    feat: include fake hand in game scene

--- Assets/Runtime/View/ClickableTile.cs
+++ Assets/Runtime/View/ClickableTile.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using Runtime.Domain;
 using UnityEngine;
 
 public class ClickableTile : MonoBehaviour
@@ -10,7 +8,7 @@ public class ClickableTile : MonoBehaviour
 
     void OnMouseDown()
     {
-        board.PlaceAt(whereIs, Card.WithTraits(new Trait("friend", Array.Empty<string>(), Array.Empty<string>())));
+        board.PlaceAt(whereIs, FindObjectOfType<FakeHand>().NextCard);
     }
 
     public void Configure(BoardRenderer board, (int x, int y) whereIs)
@@ -18,11 +16,4 @@ public class ClickableTile : MonoBehaviour
         this.board = board;
         this.whereIs = whereIs;
     }
-}
-
-public class FakeHand : MonoBehaviour
-{
-    [SerializeField] private string id;
-    [SerializeField] private List<string> friends;
-    [SerializeField] private List<string> enemies;
 }
---
+++ Assets/Runtime/View/FakeHand.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Runtime.Domain;
+using UnityEngine;
+
+public class FakeHand : MonoBehaviour
+{
+    [SerializeField] private string id;
+    [SerializeField] private List<string> friends;
+    [SerializeField] private List<string> enemies;
+    
+    public Card NextCard => Card.WithTraits(new Trait(id, friends, enemies));
+}
