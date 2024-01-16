aaddc86f48d178265135452d46e8559b076dd65f
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2024-01-16 19:09:46 +0100

    feat: fake fake hand

--- Assets/Runtime/View/ClickableTile.cs
+++ Assets/Runtime/View/ClickableTile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Runtime.Domain;
 using UnityEngine;
 
@@ -17,4 +18,11 @@ public class ClickableTile : MonoBehaviour
         this.board = board;
         this.whereIs = whereIs;
     }
+}
+
+public class FakeHand : MonoBehaviour
+{
+    [SerializeField] private string id;
+    [SerializeField] private List<string> friends;
+    [SerializeField] private List<string> enemies;
 }
