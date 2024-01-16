17b3ba368395147833b31a78f1f72d1f51ea0a2b
Iván Moreno <imoreno.owo@gmail.com>
2024-01-16 19:27:45 +0100

    feat: apply connection color to line renderer

--- Assets/Runtime/View/Connection.cs
+++ Assets/Runtime/View/Connection.cs
@@ -11,10 +11,16 @@ public class Connection : MonoBehaviour
         LineRenderer.SetPosition(0, new(points.from.x + (points.from.x * 0.2f), points.from.y + (points.from.y * 0.2f), 0));
         LineRenderer.SetPosition(1, new(points.to.x + (points.to.x * 0.2f), points.to.y + (points.to.y * 0.2f), 0));
 
-        ChangeColor(adwgf);
+        ChangeColor(ColorOf(adwgf));
     }
 
-    private Color ChangeColor(Card.Relationship adwgf)
+    void ChangeColor(Color whichOne)
+    {
+        LineRenderer.startColor = whichOne;
+        LineRenderer.endColor = whichOne;
+    }
+    
+    private Color ColorOf(Card.Relationship adwgf)
     {
         switch (adwgf)
         {
