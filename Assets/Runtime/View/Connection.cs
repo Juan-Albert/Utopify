21ad5679ae272b7ee05f1af955fc8f9777c0baf2
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2024-01-16 19:24:34 +0100

    feat: change color of connection

--- Assets/Runtime/View/Connection.cs
+++ Assets/Runtime/View/Connection.cs
@@ -1,11 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using Codice.Client.BaseCommands.BranchExplorer.Layout;
+using Runtime.Domain;
+using UnityEngine;
 
 public class Connection : MonoBehaviour
 {
     LineRenderer LineRenderer => GetComponentInChildren<LineRenderer>();
-    public void Configure(((int x, int y) from, (int x, int y) to) points)
+    public void Configure(((int x, int y) from, (int x, int y) to) points, Card.Relationship adwgf)
     {
         LineRenderer.SetPosition(0, new(points.from.x + (points.from.x * 0.2f), points.from.y + (points.from.y * 0.2f), 0));
         LineRenderer.SetPosition(1, new(points.to.x + (points.to.x * 0.2f), points.to.y + (points.to.y * 0.2f), 0));
+
+        ChangeColor(adwgf);
+    }
+
+    private Color ChangeColor(Card.Relationship adwgf)
+    {
+        switch (adwgf)
+        {
+            case Card.Relationship.Neutral:
+                return Color.gray;
+            case Card.Relationship.Positive:
+                return Color.green;
+            case Card.Relationship.Negative:
+                return Color.red;
+            default:
+                throw new NotSupportedException($"No existe una relacion entre cartas de tipo {adwgf}");
+        }
     }
 }
